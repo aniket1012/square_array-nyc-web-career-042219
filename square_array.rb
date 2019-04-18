31948856e469f8f92adef0e6a1bf2c60b5c61c7d
Aniket Patel <aniketupatel1@gmail.com>
2019-04-18 03:09:47 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,8 @@
 def square_array(array)
+  new_array = []
   array.each do |numbers|
-    math.sqrt(numbers)
+    sqaure = numbers * numbers
+    new_array.push(square)
   end
+  return new_array
 end
